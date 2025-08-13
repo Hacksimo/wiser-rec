e217733d83b3c128d281bf7649e1a9e7cceae995
HackSimo <smailbenaya@gmail.com>
2025-08-13 22:35:43 +0200

    api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
-from app.api import router
+from typing import Union
 
-app = FastAPI(title="Video Recommendation API")
-app.include_router(router)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
